6749ec18278512ffa76ea912cd584cb3fc11ee67
NomadSeele <nomad.seele@gmail.com>
2020-09-24 23:10:46 -0500

    Initial commit

---
+++ src/build/precache-manifest.5f70e00b4feef3a56ca049ce7e446918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d40dc19e40179979c56ef65643e600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "129f8ccac78870d97816",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129f8ccac78870d97816",
+    "url": "/static/js/main.bcc54619.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
